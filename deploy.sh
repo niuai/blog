c3aa8bdab33136582b5fa0f02724c5916542c4f1
Jiabin Zhou <jiabinzhou@outlook.com>
2019-12-22 21:00:30 +0800

    Change sh to deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
 npm run docs:build
 
-Set-Location docs/.vuepress/dist
+cd docs/.vuepress/dist
 
 git init
 git add -A
@@ -11,3 +16,5 @@ git commit -m 'deploy niuai-docs'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:niuai/blog.git master:gh-pages
+
+cd -
