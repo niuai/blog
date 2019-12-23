7868be94f7b13a99270853f32b6fc42c170122c0
niuai <1196454838@qq.com>
2019-12-23 23:21:33 +0800

    Auto popup docs structure.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,29 +1,17 @@
 const path = require('path')
 const fs = require('fs')
 
-/**
- * 遍历获取某一文件夹下的所有文件
- * @param {*} startPath 起始目录文件夹路径
- * @returns {Array}
- */
-function findDoc(startPath) {
-    let result = []
-    function finder(innerPath) {
-        fs.readdirSync(innerPath).forEach((file, index) => {
-            let fPath = path.join(innerPath, file)
-            let stats = fs.statSync(fPath)
-
-            if (stats.isDirectory()) finder(fPath)
-            if (stats.isFile() && fPath.endsWith('.md')) result.push(fPath)
+let docGroups = ['']
+let tempDocGroups = findDoc(path.resolve(__dirname + '/../niuai-docs/'))
+for (let index in tempDocGroups) {
+    if (tempDocGroups[index].length > 0) {
+        docGroups.push({
+            title: index,
+            collapsable: false,
+            children: tempDocGroups[index].map(filePath => filePath.substring(filePath.indexOf('niuai-docs') - 1).replace(/\\/g, '/')),
         })
     }
-    finder(startPath)
-    return result
 }
-
-let docs = findDoc(path.resolve(__dirname + '/../niuai-docs/'))
-    .filter(filePath => !filePath.endsWith('README.md'))
-    .map(filePath => filePath.substring(filePath.indexOf('niuai-docs') - 1).replace(/\\/g, '/'))
 
 module.exports = {
     title: '牛艾的博客',
@@ -60,14 +48,7 @@ module.exports = {
             '/guide/': [
                 '',
             ],
-            '/niuai-docs/': [
-                '',
-                {
-                    title: '学习笔记',
-                    collapsable: false,
-                    children: docs,
-                },
-            ],
+            '/niuai-docs/': docGroups,
         }
     },
     plugins: [
@@ -79,4 +60,27 @@ module.exports = {
         '@vuepress/medium-zoom',
         '@vuepress/nprogress'
     ]
+}
+
+/**
+ * 遍历获取某一文件夹下的所有文件，返回结果为二维数组：文件夹名称及其下的文件
+ * @param {*} startPath 起始目录文件夹路径
+ * @returns {Array}
+ */
+function findDoc(startPath) {
+    let result = new Array()
+    function finder(innerPath) {
+        let folder = innerPath.split('\\').pop()
+        result[folder] = new Array()
+
+        fs.readdirSync(innerPath).forEach((file, index) => {
+            let fPath = path.join(innerPath, file)
+            let stats = fs.statSync(fPath)
+
+            if (stats.isDirectory()) finder(fPath)
+            if (stats.isFile() && fPath.endsWith('.md') && !fPath.endsWith('README.md')) result[folder].push(fPath)
+        })
+    }
+    finder(startPath)
+    return result
 }
